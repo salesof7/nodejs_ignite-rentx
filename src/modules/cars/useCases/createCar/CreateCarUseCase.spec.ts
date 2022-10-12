d126b4391771ad1e4ea44ae0b35d84c2fa28d4a9
salesof7 <jpsaless2002@gmail.com>
2022-10-12 18:15:14 -0300

    test: ensure that cars can be registered in the app with validated registration and availability

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,63 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { CarsRepositoryInMemory } from "../../repositories/in-memory/CarsRepositoryInMemory";
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepository = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepository);
+  });
+
+  it("should be able to create a new car", async () => {
+    await createCarUseCase.execute({
+      name: "any_name",
+      description: "any_description",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "any_brand",
+      category_id: "any_category_id",
+    });
+  });
+
+  it("should not be able to create a car with exists license plate", () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "car_1",
+        description: "any_description",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "any_brand",
+        category_id: "any_category_id",
+      });
+
+      await createCarUseCase.execute({
+        name: "car_2",
+        description: "any_description",
+        daily_rate: 100,
+        license_plate: "ABC-1234",
+        fine_amount: 60,
+        brand: "any_brand",
+        category_id: "any_category_id",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to create a car with available true by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "any_car",
+      description: "any_description",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "any_brand",
+      category_id: "any_category_id",
+    });
+
+    expect(car.available).toBe(true);
+  });
+});
