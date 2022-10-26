8b6323fd767886fe4009324c7cef28e62570e5f3
salesof7 <jpsaless2002@gmail.com>
2022-10-26 18:55:35 -0300

    feat: ensure the storage provider can upload car images to Amazon S3

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorageProvider";
 import { ICarsImagesRepository } from "../../repositories/ICarsImagesRepository";
 
 interface IRequest {
@@ -9,13 +10,16 @@ interface IRequest {
 @injectable()
 class UploadCarImagesUseCase {
   constructor(
-    @inject("CarsImageRepository")
-    private carsImagesRepository: ICarsImagesRepository
+    @inject("CarsImagesRepository")
+    private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest) {
     images_name.map(async (image) => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
