12f176e6f7a8460db71a9cfdde1409bf8b93594f
salesof7 <jpsaless2002@gmail.com>
2022-10-26 18:45:05 -0300

    feat: ensure there is a storage management provider with Amazon S3 connection for user avatar upload

---
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -0,0 +1,27 @@
+import { IStorageProvider } from "../IStorageProvider";
+import fs from "fs";
+import { resolve } from "path";
+import upload from "../../../../../config/upload";
+
+class LocalStorageProvider implements IStorageProvider {
+  async save(file: string, folder: string): Promise<string> {
+    await fs.promises.rename(
+      resolve(upload.tmpFolder, file),
+      resolve(`${upload.tmpFolder}/${folder}`, file)
+    );
+    return file;
+  }
+
+  async delete(file: string, folder: string): Promise<void> {
+    const fileName = resolve(`${upload.tmpFolder}/${folder}`, file);
+
+    try {
+      await fs.promises.stat(fileName);
+    } catch (error) {
+      return;
+    }
+    await fs.promises.unlink(fileName);
+  }
+}
+
+export { LocalStorageProvider };
