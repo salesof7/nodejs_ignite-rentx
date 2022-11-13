c98bba01aead08117b166bcdc1e66048c2f66603
salesof7 <jpsaless2002@gmail.com>
2022-11-12 22:20:37 -0300

    feat: ensure email password recovery via Amazon SES

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,7 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { SESMainProvider } from "./MailProvider/implementations/SESMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
@@ -12,9 +13,14 @@ container.registerSingleton<IDateProvider>(
   DayjsDateProvider
 );
 
+const mailProvider = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMainProvider),
+};
+
 container.registerInstance<IMailProvider>(
-  "EtherealMailProvider",
-  new EtherealMailProvider()
+  "MailProvider",
+  mailProvider[process.env.MAIL_PROVIDER]
 );
 
 const diskStorage = {
